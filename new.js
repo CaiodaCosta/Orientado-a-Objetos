7b2afac283e374b7274a0de2674a4d31cee2266c
CaiodaCosta <dacostacaio22ko@gmail.com>
2024-05-13 05:06:01 -0300

    Continuando

--- new.js
+++ new.js
@@ -1,20 +1,37 @@
-function User (nome, email){
-    this.nome = nome
-    this.email = email
+// function User (nome, email){
+//     this.nome = nome
+//     this.email = email
 
-    this.exibirInfos = function () {
-        return  `${this.nome}, ${this.email}`
+//     this.exibirInfos = function () {
+//         return  `${this.nome}, ${this.email}`
+//     }
+// }
+
+// const novoUser = new User('Juliana', 'j@j.com') // forma de criar objeto com o construtor
+// console.log(novoUser.exibirInfos())
+
+// function Admin(role){
+//     User.call(this, 'Juliana', 'J@J')
+//     this.role = role || 'estudante'
+// }
+
+
+// Admin.prototype = Object.create(User.prototype)
+// const novoUser = new Admin('admin')
+
+const user = {
+    init: function(nome, email) {
+        this.nome = nome
+        this.email = email
+
+    },
+    exibirInfos: function(nome){
+        return this.nome
     }
 }
 
-const novoUser = new User('Juliana', 'j@j.com') // forma de criar objeto com o construtor
+const novoUser = Object.create(user)
+novoUser.init('Juliana', 'j@j.com')
 console.log(novoUser.exibirInfos())
-
-function Admin(role){
-    User.call(this, 'Juliana', 'J@J')
-    this.role = role || 'estudante'
-}
-
-
-Admin.prototype = Object.create(User.prototype)
-const novoUser = new Admin('admin')
+// console.log(novoUser.exibirInfos('Juliana'))
+// console.log(user.isPrototypeOf(novoUser))
