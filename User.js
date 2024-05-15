5049af4d39fbf115a4ec8e62ed3eba7ea3d41e57
CaiodaCosta <dacostacaio22ko@gmail.com>
2024-05-15 05:15:07 -0300

    Fluindo

--- User.js
+++ User.js
@@ -1,14 +1,44 @@
 export default class User {
     #nome
+    #email
+    #nascimento
+    #role
+    #ativo
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome
-        this.email = email
-        this.nascimento = nascimento
-        this.role = role || 'estudante'
-        this.ativo = ativo
+        this.#nome = nome
+        this.#email = email
+        this.#nascimento = nascimento
+        this.#role = role || 'estudante'
+        this.#ativo = ativo
     }
+
+    get nome() {
+        //logica de condicional
+        return this.#nome
+    }
+    
+    get email() {
+        //logica de condicional
+        return this.#email
+    }
+    get nascimento() {
+        //logica de condicional
+        return this.#nascimento
+    }
+    get role() {
+        //logica de condicional
+        return this.#role
+    }
+    get ativo() {
+        //logica de condicional
+        return this.#ativo
+    }
+    set nome(novoNome){ //metodo set deve ter uma propriedade
+        this.#nome = novoNome
+    }
+    
     exibirInfos() {
-        return `${this.#nome}, ${this.email}, ${this.nascimento}`
+        return `${this.nome}, ${this.email}, ${this.nascimento}`
     }
 }
 
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import Docente from "./Docente.js";
 import User from "./User.js";
 
 const novoUser = new User('Mariana', 'm@m', '2021')
+console.log(novoUser.exibirInfos())
 
-
-novoUser.#nome = 'Márcia'
+const novoAdmin = new Admin('rodrigo', 'r@r', '2021')
+console.log(novoAdmin.nome)
+novoAdmin.nome = 'André'
+console.log(novoAdmin.nome)
